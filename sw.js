6743b714357fb7c493c41ccda7c35273fbffc527
HY J <tabin8437@gmail.com>
2026-09-01 15:46:50 +0900

    서비스워커 캐시 버전 올려서 manifest.json 갱신 강제
    manifest.json은 캐시 우선 전략이라 이름을 바꿔도 서비스워커가 예전 캐시를 계속
    내려줘서 설치된 앱 이름(해외사업팀 캘린더)이 안 바뀌고 있었다. 캐시 이름을 바꿔
    기존 캐시를 무효화해서 새 manifest를 다시 받아오게 한다.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'team-calendar-v2';
+var CACHE_NAME = 'team-calendar-v3';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
