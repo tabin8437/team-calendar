682b0415edef9be75fd6621b90b5f6908faf7a55
HY J <tabin8437@gmail.com>
2026-09-09 16:29:33 +0900

    fix: 제헌절 공휴일 제거 + SW 캐시 v5 갱신 (9/28 추석 대체공휴일 반영)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'team-calendar-v4';
+var CACHE_NAME = 'team-calendar-v5';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
